76356023042042b4def415730f7343c973287897
fenderson <freddenderson@gmail.com>
2023-10-04 14:14:44 -0600

    .

---
+++ ServicesExample/src/views/PostDetailView.vue
@@ -0,0 +1,28 @@
+<template>
+    <h3>Post Detail</h3>
+    <div>
+        <h3>{{ post.title }}</h3>
+        <p>{{ post.body }}</p>
+        <Router-Link :to="{ name: 'PostList' }">Volver atras</Router-Link>
+    </div>
+</template>
+
+<script setup>
+import postService from '../services/service';
+import { RouterLink, useRoute } from 'vue-router';
+import { onMounted } from 'vue';
+
+const service = new postService();
+
+const post = service.getpost();
+
+onMounted(async ()=>{
+    const route = useRoute()
+    let elm = route.params.id
+    await service.fetchById(elm)
+})
+</script>
+
+
+
+<style scoped></style>
---
+++ ServicesExample/src/views/PostListView.vue
@@ -0,0 +1,29 @@
+<template>
+    <h3>Post List</h3>
+    <ul>
+        <li v-for="post in posts" :key="post.id">
+            <Router-Link :to="{ name: 'PostDetail', params: { id: post.id } }">
+                {{ post.title }}
+            </Router-Link>
+
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+import postService from '../services/service';
+import { onMounted } from 'vue';
+
+const service = new postService();
+
+
+const posts = service.getPosts();
+
+onMounted(async () => {
+    await service.fetchAll();
+})
+
+</script>
+
+<style scoped></style>
